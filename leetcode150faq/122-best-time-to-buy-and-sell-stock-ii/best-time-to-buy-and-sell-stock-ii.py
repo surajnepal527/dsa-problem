d5ded3256d22983509e4b177782c4fe70c1a8857
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-06-09 07:25:01 -0600

    Time: 22 ms (99.92%) | Memory: 12.8 MB (32.97%) - LeetSync

--- leetcode150faq/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ leetcode150faq/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -7,7 +7,7 @@ class Solution(object):
         if len(prices) == 1:
             return 0
         profit = 0
-        for i in range(0, len(prices) - 1):
+        for i in range(len(prices) - 1):
             if prices[i] < prices[i + 1]:
                 profit += prices[i+1] - prices[i]
         return profit
