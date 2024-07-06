bbac0bda363a4e4881354a97d7144740f51e2a38
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-07-06 12:57:48 -0600

    Time: 191 ms (43.75%) | Memory: 22 MB (52.65%) - LeetSync

--- leetcode150faq/209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ leetcode150faq/209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -9,22 +9,16 @@ class Solution(object):
         min_count = n + 1
         left = 0
         cur_sum = 0
-        for right in range(n):
-            
+        for right in range(n):  
             cur_sum += nums[right]
-            
             while cur_sum >= target:
                 min_count = min(min_count, right - left + 1)
                 cur_sum -= nums[left]
                 left += 1
-
         if min_count == n + 1:
             return 0
         else:
             return min_count
-            
-            
-            return min_count if min_count <= n else 0
 
 
         
