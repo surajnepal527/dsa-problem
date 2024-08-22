a3bfa526857e9890695b4eacde6f358f373c2380
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-08-22 11:38:21 -0600

    Time: 167 ms (38.95%) | Memory: 18.2 MB (8.48%) - LeetSync

--- leetcode150faq/1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ leetcode150faq/1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -10,15 +10,14 @@ class Solution(object):
         for i in range(len(s)):
             if s[i] != '(' and s[i] != ')':
                 ans.append(s[i])
+            elif s[i] == '(':
+                spc += 1
+                ans.append(s[i])
+            elif s[i] == ')' and spc > epc:
+                epc += 1
+                ans.append(s[i])
             else:
-                if s[i] == '(':
-                    spc += 1
-                    ans.append(s[i])
-                elif s[i] == ')' and spc > epc:
-                    epc += 1
-                    ans.append(s[i])
-                else:
-                    continue
+                continue
         
         if spc == epc:
             return ''.join(ans)
@@ -29,20 +28,12 @@ class Solution(object):
             for i in range(len(ans)-1, -1, -1):
                 if ans[i] != '(' and ans[i] != ')':
                     nans.append(ans[i])
+                elif ans[i] == ')':
+                    nepc += 1
+                    nans.append(ans[i])
+                elif ans[i] == '(' and nspc < nepc:
+                    nspc += 1
+                    nans.append(ans[i])
                 else:
-                    if ans[i] == ')':
-                        nepc += 1
-                        nans.append(ans[i])
-                    elif ans[i] == '(' and nspc < nepc:
-                        nspc += 1
-                        nans.append(ans[i])
-                    else:
-                        continue
+                    continue
             return ''.join(reversed(nans))
-            # fans = []
-            # for i in range(len(nans)-1, -1, -1):
-            #     fans.append(nans[i])
-            
-            # return ''.join(fans)
-
-
