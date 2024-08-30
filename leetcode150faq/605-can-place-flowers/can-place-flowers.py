b1cd0436cdd597c957d8b72163f625000ac3c85b
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-08-30 07:04:19 -0600

    Time: 114 ms (74.02%) | Memory: 12.3 MB (58.62%) - LeetSync

--- leetcode150faq/605-can-place-flowers/can-place-flowers.py
+++ leetcode150faq/605-can-place-flowers/can-place-flowers.py
@@ -7,24 +7,16 @@ class Solution(object):
         """
         if n == 0:
             return True
-            
+
         if len(flowerbed) == 1:
             if flowerbed[0] == 0 and n == 1:
-                return True
-            elif flowerbed[0] == 1 and n > 0:
-                return False
-            elif flowerbed[0] == 1 and n == 0:
                 return True
             else:
                 return False
             
-        if flowerbed[0] == 0:
-            if flowerbed[1] == 0:
-                flowerbed[0] = 1
-                n -= 1
-        for i in range(1, len(flowerbed)):
-            if flowerbed[i-1] == 0 and flowerbed[i] == 0 and n > 0:
-                if i == len(flowerbed) - 1 or flowerbed[i+1] == 0:
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0 and n > 0:
+                if (i == 0 and flowerbed[i+1] == 0) or (i == len(flowerbed) - 1 and flowerbed[i-1] == 0) or (flowerbed[i-1] == 0 and flowerbed[i+1] == 0):
                     flowerbed[i] = 1
                     n -= 1
         
