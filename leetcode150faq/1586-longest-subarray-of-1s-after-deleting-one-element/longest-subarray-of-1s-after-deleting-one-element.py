ecab014d345f9756b99e01e37ddbd68de2c3fd0d
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-01 12:26:39 -0600

    Time: 465 ms (61.86%) | Memory: 17.4 MB (34.19%) - LeetSync

--- leetcode150faq/1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ leetcode150faq/1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -5,10 +5,8 @@ class Solution(object):
         :rtype: int
         """
         left = zero_count= max_len = 0
-        all_ones = True
         for right in range(len(nums)):
             if nums[right] == 0:
-                all_ones = False
                 zero_count += 1
             while zero_count > 1:
                 if nums[left] == 0:
@@ -18,8 +16,6 @@ class Solution(object):
             max_len = max(max_len, right - left)
         
         return max_len
-
-        #return max_len if not all_ones else max_len - 1
 
 
         
