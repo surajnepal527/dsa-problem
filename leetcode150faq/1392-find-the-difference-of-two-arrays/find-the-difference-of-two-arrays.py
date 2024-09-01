eed7fbfd5d054d8ade31bdc5d3dbc87b13685a06
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-01 14:56:31 -0600

    Time: 137 ms (40.75%) | Memory: 12 MB (75.14%) - LeetSync

---
+++ leetcode150faq/1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
+        """
+        set_num1 = set()
+        set_num2 = set()
+        ans_num1 = set()
+        ans_num2 = set()
+        for num1 in nums1:
+            set_num1.add(num1)
+        for num2 in nums2:
+            set_num2.add(num2)
+
+        for num1 in nums1:
+            if num1 not in set_num2:
+                ans_num1.add(num1)
+        for num2 in nums2:
+            if num2 not in set_num1:
+                ans_num2.add(num2)
+        
+        return [list(ans_num1),list(ans_num2)]
+        
+
+
