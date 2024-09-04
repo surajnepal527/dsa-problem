a3eb3c45a778e4c1fbe951035f1451a513d15ade
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-03 18:11:32 -0600

    Time: 665 ms (90.89%) | Memory: 51.5 MB (90.31%) - LeetSync

--- leetcode150faq/2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
+++ leetcode150faq/2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
@@ -10,30 +10,20 @@ class Solution(object):
         :rtype: int
         """
         prev = None
-        slow = fast = head
+        slow , fast = head , head
         while fast and fast.next:
-            prev = slow
-            slow = slow.next
             fast = fast.next.next
-
-        
-        head2 = prev.next
-        prev.next = None
-        cur = head
-        prev = None
-        while cur:
-            next = cur.next
-            cur.next = prev
-            prev = cur
-            cur = next
-        
-        head1 = prev
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
+            
 
         max_sum = -1
-        while head1 and head2:
-            max_sum = max(max_sum, head1.val + head2.val)
-            head1 = head1.next
-            head2 = head2.next
+        while prev and slow:
+            max_sum = max(max_sum, prev.val + slow.val)
+            prev = prev.next
+            slow = slow.next
         
         return max_sum
  
