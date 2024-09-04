06ababb82667aff8265f67ca9229058b2bc3a511
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-03 18:00:49 -0600

    Time: 655 ms (91.71%) | Memory: 51.4 MB (90.31%) - LeetSync

--- leetcode150faq/2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
+++ leetcode150faq/2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
@@ -1,4 +1,3 @@
-import sys
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
@@ -10,39 +9,34 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: int
         """
-        old_to_new = {}
-        current = head
-        while current:
-            old_to_new[current] = ListNode(current.val)
-            current = current.next
-    
-        current = head
-        while current:
-            if current.next:
-                old_to_new[current].next = old_to_new[current.next]
-            current = current.next
-
-        new_current = old_to_new[head]
         prev = None
-        while new_current:
-            next = new_current.next
-            new_current.next = prev
-            prev = new_current
-            new_current = next
+        slow = fast = head
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+
         
-        new_head = prev
-        max_sum = 0
-        cur_sum = 0
-        while head and new_head:
-            cur_sum = head.val + new_head.val
-            max_sum = max(max_sum, cur_sum)
-            head = head.next
-            new_head = new_head.next
+        head2 = prev.next
+        prev.next = None
+        cur = head
+        prev = None
+        while cur:
+            next = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next
+        
+        head1 = prev
+
+        max_sum = -1
+        while head1 and head2:
+            max_sum = max(max_sum, head1.val + head2.val)
+            head1 = head1.next
+            head2 = head2.next
         
         return max_sum
+ 
 
-
-
-
-    
-        
+            
+            
