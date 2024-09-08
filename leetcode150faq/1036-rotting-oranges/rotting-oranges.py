f6fd63c7b3b7d52067d1ac1c7d74f60bed691ef9
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-08 16:59:46 -0600

    Time: 34 ms (35.28%) | Memory: 11.6 MB (40.90%) - LeetSync

--- leetcode150faq/1036-rotting-oranges/rotting-oranges.py
+++ leetcode150faq/1036-rotting-oranges/rotting-oranges.py
@@ -8,12 +8,19 @@ class Solution(object):
         row_len = len(grid)
         col_len = len(grid[0])
         queue = deque()
+        fresh_count = 0
         #lets find first rotten orange
         for row in range(row_len):
             for col in range(col_len):
                 if grid[row][col] == 2:
                     queue.append((row, col, 0))
-                               
+                elif grid[row][col] == 1:
+                    fresh_count += 1
+
+
+        if fresh_count == 0:
+            return 0
+
         directions = [(-1,0),(0,1),(1,0),(0,-1)]
         max_step = 0
         while queue:
@@ -25,12 +32,8 @@ class Solution(object):
                 if 0 <= new_row < row_len and 0 <= new_col < col_len and grid[new_row][new_col] == 1:
                     grid[new_row][new_col] = 2
                     queue.append((new_row, new_col, steps+1))
+                    fresh_count -= 1
 
         #check if there are any fresh orange left
-        for r in range(row_len):
-            for c in range(col_len):
-                if grid[r][c] == 1:
-                    return -1
-        
-        return max_step           
+        return -1 if fresh_count > 0 else max_step          
         
