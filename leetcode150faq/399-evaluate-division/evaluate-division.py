4f90a6c62b85f9bf2e25ca399a828bef7268f9fb
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-08 13:16:56 -0600

    Time: 5 ms (98.46%) | Memory: 11.7 MB (82.11%) - LeetSync

---
+++ leetcode150faq/399-evaluate-division/evaluate-division.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+class Solution(object):
+    def calcEquation(self, equations, values, queries):
+        """
+        :type equations: List[List[str]]
+        :type values: List[float]
+        :type queries: List[List[str]]
+        :rtype: List[float]
+        """
+        adj_list = defaultdict(list)
+        for index, variables in enumerate(equations):
+            src = variables[0]
+            des = variables[1]
+            val = values[index] 
+            adj_list[src].append([des, val])
+            adj_list[des].append([src, 1/val])
+        
+        ans = [-1.0] * len(queries)
+        res = 1
+        for i , (u, v)  in enumerate(queries):
+            if u not in adj_list or v not in adj_list:
+                continue
+
+            visited = self.setVisited(adj_list)
+            res = self.dfs(u, v, adj_list,visited, 1.0)
+            if res != -1.0:
+                ans[i] = res
+        
+        return ans
+    
+    def dfs(self, src, des, adj_list, visited, res):
+        if src == des:
+            return res
+
+        visited[src] = True
+
+        for neig,val in adj_list[src]:
+            if not visited[neig]:
+                result = self.dfs(neig,des, adj_list, visited, res * val)
+                if result != -1.0:
+                    return result
+            
+        return -1.0
+        
+    def setVisited(self, adj_list):
+        return {node:False for node in adj_list}
+
+
+
+
+
+        
