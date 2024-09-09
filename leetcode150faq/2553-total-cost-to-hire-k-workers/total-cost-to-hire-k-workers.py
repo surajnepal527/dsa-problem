5b94b10551f3154177ba1c318d9070df0855c2d0
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-09 14:14:39 -0600

    Time: 1108 ms (19.29%) | Memory: 20.2 MB (70.56%) - LeetSync

---
+++ leetcode150faq/2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.py
@@ -0,0 +1,46 @@
+import heapq
+import sys
+class Solution(object):
+    def totalCost(self, costs, k, candidates):
+        """
+        :type costs: List[int]
+        :type k: int
+        :type candidates: int
+        :rtype: int
+        """
+        ans = 0
+        i = 0 
+        j = len(costs) - 1
+        hire = 0
+        min_heap_left = []
+        min_heap_right = []
+        while hire < k:
+            while len(min_heap_left) < candidates and i <= j:
+                heapq.heappush(min_heap_left, costs[i])
+                i += 1
+            while len(min_heap_right) < candidates and j >= i:
+                heapq.heappush(min_heap_right, costs[j])
+                j -= 1
+
+            left_min = - 1
+            right_min = -1
+            if not min_heap_left:
+                left_min = sys.maxsize
+            else:
+                left_min = min_heap_left[0]
+            
+            if not min_heap_right:
+                right_min = sys.maxsize
+            else:
+                right_min = min_heap_right[0]
+            
+            if left_min <= right_min:
+                ans += left_min
+                heapq.heappop(min_heap_left)
+            else:
+                ans += right_min
+                heapq.heappop(min_heap_right)
+            
+            hire += 1
+
+        return ans        
