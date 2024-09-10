b4f1e381c23dcb10233fdd744d79c3c3c4785cf8
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-10 17:03:46 -0600

    Time: 7 ms (93.76%) | Memory: 11.5 MB (92.40%) - LeetSync

--- leetcode150faq/1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ leetcode150faq/1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -8,13 +8,16 @@ class Solution(object):
             return 0
         if n <= 2:
             return 1
-        t = [-1] * (n+1)
-        t[0] = 0
-        t[1] = 1
-        t[2] = 1
-
+            
+        a = 0
+        b = 1
+        c = 1
+        d = 1
         for i in range(3, n+1, 1):
-            t[i] = t[i-1] + t[i-2] + t[i-3]
+            d = a+b+c
+            a = b
+            b = c
+            c = d
         
-        return t[n]
+        return d
         
