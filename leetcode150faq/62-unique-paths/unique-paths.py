025a00ea9691524d97b4d19a21bd40a4e5ea67ad
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-11 13:28:57 -0600

    Time: 20 ms (18.19%) | Memory: 11.5 MB (93.94%) - LeetSync

---
+++ leetcode150faq/62-unique-paths/unique-paths.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        t = [[-1 for _ in range(n)] for _ in range(m)]
+        return self.solve(0, 0, m , n, t)
+    
+    def solve(self, row, col , m , n,t):
+        if row >= m or col >= n:
+            return 0
+        
+        if row == m-1 and col == n-1:
+            return 1
+        
+        if t[row][col] != -1:
+            return t[row][col]
+        
+        #go down and go right
+        t[row][col] =  self.solve(row+1, col, m , n,t ) + self.solve(row, col+1, m , n, t)
+        return t[row][col]
+        
+        
