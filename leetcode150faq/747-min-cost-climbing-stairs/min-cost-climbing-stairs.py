db3f3e22466d81986d8e9948d4ddc60a6a2e89ec
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-10 21:02:30 -0600

    Time: 34 ms (60.82%) | Memory: 11.6 MB (84.20%) - LeetSync

--- leetcode150faq/747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ leetcode150faq/747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -9,6 +9,6 @@ class Solution(object):
         tmp[0] = cost[0]
         tmp[1] = cost[1]
         for i in range(2, n,1):
-            tmp[i] = cost[i] + min(tmp[i-1], tmp[i-2])
+            cost[i] = cost[i] + min(cost[i-1], cost[i-2])
         
-        return min(tmp[n-1], tmp[n-2])
+        return min(cost[n-1], cost[n-2])
