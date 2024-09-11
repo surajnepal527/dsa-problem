6f24a7b905458bab2a65e95bda05a946837b3845
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-11 16:52:25 -0600

    Time: 2202 ms (9.34%) | Memory: 22 MB (86.85%) - LeetSync

---
+++ leetcode150faq/1250-longest-common-subsequence/longest-common-subsequence.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+        tmp = [[-1 for _ in range(1001)] for _ in range(1001)]
+        return self.solve(0 , 0,text1, text2, tmp)
+    
+    def solve(self, idx1, idx2, text1, text2, tmp):
+        if idx1 >= len(text1) or idx2 >= len(text2):
+            return 0
+        
+        if tmp[idx1][idx2] != -1:
+            return tmp[idx1][idx2]
+
+        if text1[idx1] == text2[idx2]:
+            tmp[idx1][idx2] = 1 + self.solve(idx1+1, idx2+1, text1, text2,tmp)
+            return tmp[idx1][idx2]
+        else:
+           tmp[idx1][idx2] =  max(self.solve(idx1, idx2+1, text1, text2, tmp), self.solve(idx1+1, idx2, text1, text2,tmp)) 
+           return tmp[idx1][idx2]        
