4f02e06e987d7bfba6165b2ff4bce35422e8e73f
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-11 21:28:48 -0600

    Time: 1453 ms (5.11%) | Memory: 13.4 MB (43.10%) - LeetSync

--- leetcode150faq/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ leetcode150faq/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -4,27 +4,18 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        t = [[-1 for _ in range(2)] for _ in range(5001)]
-        buy = True
-        return self.solve(prices,0,buy, t)
-
-    def solve(self, prices, day, buy, t):
-        if day >= len(prices):
+        
+        n = len(prices)
+        if n == 0 or n == 1:
             return 0
-        
-        if t[day][buy] != -1:
-            return t[day][buy]
+        t = [-1 for _ in range(50001)]
+        t[0] = 0
+        t[1] = max(prices[1]- prices[0], 0)
+        for i in range(2, n): 
+            t[i] = t[i-1]
+            for j in range(i):
+                today_profit = prices[i] - prices[j]
+                prev_profit = t[j-2] if j >= 2 else 0
+                t[i] = max(t[i], today_profit + prev_profit)
 
-        profit = 0
-        if buy:
-            take = self.solve(prices,day+1, False, t) - prices[day]
-            not_take = self.solve(prices,day+1, True, t)
-            profit =  max(profit,take, not_take)
-        else:
-            sell = prices[day] + self.solve(prices, day+2, True, t)
-            not_sell= self.solve(prices, day+1, False, t)
-            profit =  max(profit, sell, not_sell)
-        
-        t[day][buy] = profit
-        return t[day][buy]
-        
+        return t[n-1]
