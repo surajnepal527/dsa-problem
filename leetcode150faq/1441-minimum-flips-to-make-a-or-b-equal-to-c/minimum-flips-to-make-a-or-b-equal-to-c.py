e761e00d5c71bb54b844f6fabf362efd649c39e9
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-12 16:55:32 -0600

    Time: 13 ms (60.12%) | Memory: 11.6 MB (74.54%) - LeetSync

--- leetcode150faq/1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.py
+++ leetcode150faq/1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -6,19 +6,18 @@ class Solution(object):
         :type c: int
         :rtype: int
         """
-        flip = 0
-        while c != 0 or b != 0 or a != 0:
-            if c & 1 == 1:
-                if a & 1 == 0 and b & 1 == 0:
-                    flip += 1
-            else:
-                if a & 1 == 1:
-                    flip += 1
-                if b & 1 == 1:
-                    flip += 1
-            
-            a = a >> 1
-            b = b >> 1
-            c = c >> 1
-        return flip
+        tmp = (a|b)^c
+        res = (a&b)&tmp
+        ans = 0
+        while tmp!=0:
+            if tmp&1 == 1:
+                ans += 1
+            tmp = tmp>>1
+        
+        while res != 0:
+            if res&1 == 1:
+                ans += 1
+            res = res>>1
+        
+        return ans
 
