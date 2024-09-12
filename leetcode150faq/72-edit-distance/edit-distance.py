c98b617be424b7a9a5854c39e93476992aff0ae3
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-12 12:18:32 -0600

    Time: 119 ms (13.82%) | Memory: 15 MB (71.35%) - LeetSync

--- leetcode150faq/72-edit-distance/edit-distance.py
+++ leetcode150faq/72-edit-distance/edit-distance.py
@@ -5,31 +5,16 @@ class Solution(object):
         :type word2: str
         :rtype: int
         """
-        m,n = len(word1), len(word2)
-        t = [[-1 for _ in range(502)] for _ in range(502)]
-        return self.solve(0,0,word1, word2, t)
-    
-    def solve(self, idx1, idx2, w1, w2, t):
-        if idx1 >= len(w1):
-            return len(w2)- idx2
-        
-        if idx2 >= len(w2):
-            return len(w1) - idx1
-        
-        if t[idx1][idx2] != -1:
-            return t[idx1][idx2]
+        m, n  = len(word1), len(word2)
+        t = [[-1 for _ in range(n+1)] for _ in range(m+1)]
 
-        if w1[idx1] == w2[idx2]:
-          t[idx1][idx2] = self.solve(idx1+1, idx2+1, w1, w2, t)
-          return t[idx1][idx2]
-        else:
-            replace = 1 + self.solve(idx1+1, idx2+1, w1, w2, t)
-            delete = 1 + self.solve(idx1+1, idx2, w1, w2, t)
-            insert = 1 + self.solve(idx1, idx2+1, w1, w2, t)
-            t[idx1][idx2] =  min(replace, delete, insert)
-            return t[idx1][idx2]
+        for i in range(m+1):
+            for j in range(n+1):
+                if i == 0 or j == 0:
+                    t[i][j] = i + j
+                elif word1[i-1] == word2[j-1]:
+                    t[i][j] = t[i-1][j-1]
+                else:
+                    t[i][j] = 1+ min(t[i][j-1],t[i-1][j], t[i-1][j-1])
         
-        return -1
-        
-           
-        
+        return t[m][n]
