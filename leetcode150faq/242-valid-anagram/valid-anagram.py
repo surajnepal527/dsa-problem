3796be36b61767f01efc60379d29e3012298452b
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-14 10:54:10 -0600

    Time: 54 ms (30.54%) | Memory: 17 MB (46.66%) - LeetSync

---
+++ leetcode150faq/242-valid-anagram/valid-anagram.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #frequency array
+        char_arr = [0]*26
+        for i in range(len(s)):
+            cur_idx = ord(s[i]) - ord('a')
+            char_arr[cur_idx] += 1
+        
+        for j in range(len(t)):
+            cur_idx = ord(t[j]) - ord('a')
+            char_arr[cur_idx] -=1
+            if char_arr[cur_idx] == -1:
+                return False
+        
+        for k in range(26):
+            if char_arr[k] != 0:
+                return False
+        
+        return True
+        
