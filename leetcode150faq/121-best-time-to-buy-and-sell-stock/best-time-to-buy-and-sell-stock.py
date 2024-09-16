475f7e67782bd680583c489e14f5e16fea52e428
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-16 16:55:36 -0600

    Time: 817 ms (6.52%) | Memory: 27.5 MB (37.36%) - LeetSync

--- leetcode150faq/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ leetcode150faq/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,18 +1,13 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        min_price = prices[0]
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) <= 1:
+            return 0
+        min_so_far = prices[0]
         max_profit = 0
-        for i in range(len(prices)):
-            if prices[i] < min_price:
-                min_price = prices[i]
-            max_profit = max(max_profit, prices[i] - min_price)
+        for i in range(1, len(prices)):
+            cur_profit = prices[i] - min_so_far
+            max_profit = max(max_profit, cur_profit)
+            min_so_far = min(min_so_far, prices[i])
         
         return max_profit
-
-
-
         
