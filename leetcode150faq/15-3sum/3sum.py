a59bc15a564f05921588603e0e7f86c01a606969
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-16 10:51:37 -0600

    Time: 684 ms (65.78%) | Memory: 20.7 MB (71.79%) - LeetSync

--- leetcode150faq/15-3sum/3sum.py
+++ leetcode150faq/15-3sum/3sum.py
@@ -16,9 +16,9 @@ class Solution:
                     left += 1
                 else:
                     ans.append([nums[left], nums[right], nums[i]])
-                    left += 1
-                    while nums[left] == nums[left -1] and left < right:
-                        left += 1
+                    right -= 1
+                    while nums[right] == nums[right + 1] and left < right:
+                        right -= 1
         return ans
                     
 
