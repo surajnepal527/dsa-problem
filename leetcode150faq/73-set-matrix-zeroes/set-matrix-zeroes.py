011bc4dbed5a53cd32a3fd0dd3449a08d44dd849
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-16 20:23:13 -0600

    Time: 106 ms (37.10%) | Memory: 17.3 MB (98.12%) - LeetSync

--- leetcode150faq/73-set-matrix-zeroes/set-matrix-zeroes.py
+++ leetcode150faq/73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,52 +1,43 @@
-class Solution(object):
-    def setZeroes(self, matrix):
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
         """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
+        Do not return anything, modify matrix in-place instead.
         """
-        #First col matrix matrix[0][col]
-        #First row matrix matrix[row][0] 
-        rows = len(matrix)
-        cols = len(matrix[0])
-
-                #check for row 0
-        setRow = False
-        for row in range(rows):
-            if matrix[row][0] == 0:
-                setRow = True
-                continue
-
-        setCol = False
-        for col in range(cols):
+        first_row_zero = False
+        first_col_zero = False
+        row_len = len(matrix)
+        col_len = len(matrix[0])
+        #first row check
+        for col in range(col_len):
             if matrix[0][col] == 0:
-                setCol = True
-                continue
-
-        for row in range(1, rows):
-            for col in range(1, cols):
+                first_row_zero = True
+                break
+        #first col check
+        for row in range(row_len):
+            if matrix[row][0] == 0:
+                first_col_zero = True
+        #Iterate over the entire matrix and set first row and col to zer
+        #as per inter content
+        for row in range(1, row_len):
+            for col in range(1, col_len):
                 if matrix[row][col] == 0:
-                    matrix[0][col] = matrix[row][0] = 0
+                    matrix[0][col] = 0
+                    matrix[row][0] = 0
+        
 
-
-        for row in range(1, rows):
-            for col in range(1, cols):
+        for row in range(1, row_len):
+            for col in range(1, col_len):
                 if matrix[row][0] == 0 or matrix[0][col] == 0:
                     matrix[row][col] = 0
         
-
-        
-        if setRow == True:
-            for row in range(rows):
-                matrix[row][0] = 0
-        
-        if setCol == True:
-            for col in range(cols):
+        if first_row_zero:
+            for col in range(col_len):
                 matrix[0][col] = 0
-        
-        return matrix
+        if first_col_zero:
+            for row in range(row_len):
+                matrix[row][0] = 0
+
+            
 
 
-
-
-
-        
+    
