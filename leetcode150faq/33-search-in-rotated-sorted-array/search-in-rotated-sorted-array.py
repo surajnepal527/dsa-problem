e6d6a2373e212d99898815daf60634a27dd05498
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-21 15:23:25 -0600

    Time: 43 ms (57.61%) | Memory: 17 MB (5.50%) - LeetSync

--- leetcode150faq/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ leetcode150faq/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -16,26 +16,48 @@ class Solution:
         
         left_arr = nums[0:min_idx]
         right_arr = nums[min_idx:len(nums)]
-        left_left = 0 
-        left_right = len(left_arr) -1
-        while left_left <= left_right:
-            left_mid = (left_left+left_right)//2
-            if left_arr[left_mid] > target:
-                left_right = left_mid - 1
-            elif left_arr[left_mid] < target:
-                left_left = left_mid + 1
-            else:
-                return left_mid
+        result = self.binarySearch(left_arr, target)
+        if result != -1:
+            return result
+        result = self.binarySearch(right_arr, target)
+        if result != -1:
+            return result + min_idx
         
-        right_left = 0
-        right_right = len(right_arr) -1
-        while right_left <= right_right:
-            right_mid = (right_left + right_right)//2
-            if right_arr[right_mid] < target:
-                right_left = right_mid + 1
-            elif right_arr[right_mid] > target:
-                right_right = right_mid - 1
-            else:
-                return right_mid+min_idx
+        return -1
+
+        # left_left = 0 
+        # left_right = len(left_arr) -1
+        # while left_left <= left_right:
+        #     left_mid = (left_left+left_right)//2
+        #     if left_arr[left_mid] > target:
+        #         left_right = left_mid - 1
+        #     elif left_arr[left_mid] < target:
+        #         left_left = left_mid + 1
+        #     else:
+        #         return left_mid
         
+        # right_left = 0
+        # right_right = len(right_arr) -1
+        # while right_left <= right_right:
+        #     right_mid = (right_left + right_right)//2
+        #     if right_arr[right_mid] < target:
+        #         right_left = right_mid + 1
+        #     elif right_arr[right_mid] > target:
+        #         right_right = right_mid - 1
+        #     else:
+        #         return right_mid+min_idx
+        
+        # return -1
+
+    def binarySearch(self, nums:List[int], target:int)-> int:
+        left, right = 0, len(nums)-1
+        while left<=right:
+            mid = (left+right)//2
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid +1
+            else:
+                return mid
+
         return -1
