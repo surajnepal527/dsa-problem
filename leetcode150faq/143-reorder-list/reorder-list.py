1615baf5514cc8dfd6d261ca6b8a60e71cfbcf44
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-23 05:50:41 -0600

    Time: 57 ms (48.40%) | Memory: 24.7 MB (40.57%) - LeetSync

--- leetcode150faq/143-reorder-list/reorder-list.py
+++ leetcode150faq/143-reorder-list/reorder-list.py
@@ -8,44 +8,33 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        #find the total length
-        n, cur, headm = 0, head, head
-        while cur:
-            cur = cur.next
-            n += 1
-        mid = n//2
-
-        #lets find the mid point
-        m = 0
-        n_cur = head
-        prev_one = None
-        while m <= mid:
-            prev_one = n_cur
-            n_cur = n_cur.next
-            m += 1
-
-        n_mid = n_cur
-        prev_one.next = None
-        #let revers it 
-        prev_two = None
-        while n_mid:
-            n_mid_next = n_mid.next
-            n_mid.next = prev_two
-            prev_two = n_mid
-            n_mid = n_mid_next
-        # now let switch the pointer as per questions
-        headn = prev_two
-        cur_main = headm
-        cur_new = headn
-        while cur_main and cur_new:
-            next_main = cur_main.next
-            next_new = cur_new.next
-            cur_main.next = cur_new
-            cur_new.next = next_main
-            cur_main = next_main
-            cur_new = next_new    
         
+        #lets try this with slow fast pointer method
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
         
+        prev = None
+        cur_n = slow.next
+        slow.next = None
+        
+        while cur_n:
+            next = cur_n.next
+            cur_n.next = prev
+            prev = cur_n
+            cur_n = next
+
+        head_n = prev
+        head_m = head
+        while head_n and head_m:
+            m_next = head_m.next
+            n_next = head_n.next
+            head_m.next = head_n
+            head_n.next = m_next
+            head_m = m_next
+            head_n = n_next
 
 
 
