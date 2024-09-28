c56c72c9094a23d36d4c89e92e5b7e03f7492ca1
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-28 09:22:50 -0600

    Time: 458 ms (69.66%) | Memory: 30 MB (18.92%) - LeetSync

--- leetcode150faq/215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ leetcode150faq/215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,42 +1,14 @@
-class Solution(object):
-    def findKthLargest(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        length = len(nums)
-        L = 0
-        R = length - 1
-        privot_idx = L
-
-        while(True):
-            pivot_idx = self.getPivotIndex(nums, L, R)
-            if pivot_idx == k -1:
-                break
-            elif pivot_idx < k-1:
-                L = pivot_idx+1
-            else:
-                R = pivot_idx - 1
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        max_heap = []
+        for num in nums:
+            if len(max_heap) < k:
+                heapq.heappush(max_heap, num)
+            elif max_heap[0] < num:
+                heapq.heappop(max_heap)
+                heapq.heappush(max_heap, num)
         
-        return nums[pivot_idx]
-    
-    def getPivotIndex(self,nums, L , R):
-        i = L+1
-        j = R
-        pivot_idx = L
-        while i <= j:
-            if nums[i] < nums[pivot_idx] and nums[j] > nums[pivot_idx]:
-                nums[i], nums[j] = nums[j], nums[i]
-                i +=- 1
-                j -= 1
-            
-            if nums[i] >= nums[pivot_idx]:
-                i += 1
-            
-            if nums[j] <= nums[pivot_idx]:
-                j -= 1
-        
-        nums[j], nums[pivot_idx] = nums[pivot_idx], nums[j]
-        return j
+        return max_heap[0]
 
+        
