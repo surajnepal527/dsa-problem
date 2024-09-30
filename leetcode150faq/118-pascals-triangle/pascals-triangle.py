65f4888b2d690335d85bb1e860efce74d806ecd7
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-09-30 06:43:54 -0600

    Time: 46 ms (5.84%) | Memory: 16.7 MB (5.56%) - LeetSync

--- leetcode150faq/118-pascals-triangle/pascals-triangle.py
+++ leetcode150faq/118-pascals-triangle/pascals-triangle.py
@@ -1,32 +1,27 @@
-class Solution(object):
-    def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
-        l1 = [1]
-        l2 = [1,1]
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = []
         ans = []
         if numRows == 1:
-            ans.append(l1)
-            return ans
+            res.append([1])
+            return res
         elif numRows == 2:
-            ans.append(l1)
-            ans.append(l2)
-            return ans
+            res.append([1])
+            res.append([1,1])
+            return res
         else:
-            ans.append(l1)
-            ans.append(l2)
-            for i in range(2, numRows):
-                tmp_lst = []
-                for j in range(i+1):
-                    if j == 0 or j == i:
-                        tmp_lst.append(1)
-                    else:
-                        tmp_lst.append(ans[i-1][j-1] + ans[i-1][j])
-                ans.append(tmp_lst)
-            return ans
-            
+            res.append([1])
+            res.append([1,1])
+            for n in range(3,numRows+1):
+                ans = []
+                tmp = 1
+                ans.append(tmp)
+                for i in range(1, n):
+                    tmp = tmp * (n-i)
+                    tmp = tmp/i
+                    ans.append(int(tmp))
+                res.append(ans.copy())
+        return res
 
-
+        
         
