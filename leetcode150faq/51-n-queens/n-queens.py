644483832c76b5417ac986cebcfa31d8b62a375c
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-10-02 09:10:50 -0600

    Time: 48 ms (77.05%) | Memory: 17.1 MB (18.67%) - LeetSync

--- leetcode150faq/51-n-queens/n-queens.py
+++ leetcode150faq/51-n-queens/n-queens.py
@@ -1,43 +1,23 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        res = []
-        ans = []
-        board = [["." for _ in range(n)] for _ in range(n) ]
-        def backtrack(row, board):
+        #initialize board
+        board = [["." for _ in range (n)] for _ in range(n)]
+        res, col_set, left_dia_set, right_dia_set = [], set(), set(),set()
+        def backtrack(row):
             if row == n:
-                 res.append(["".join(board[k]) for k in range(n)])
-                 return
+                res.append(["".join(board[k]) for k in range(n)])
             for col in range(n):
-                if self.isValid(board, row, col):
-                    board[row][col] = "Q"
-                    backtrack(row+1, board)
-                    board[row][col] = "."
-        
-        backtrack(0, board)
+                if col not in col_set and (row-col) not in left_dia_set and (row+col) not in right_dia_set:
+                    board[row][col]= "Q"
+                    col_set.add(col)
+                    left_dia_set.add(row-col)
+                    right_dia_set.add(row+col)
+                    backtrack(row+1)
+                    board[row][col]= "."
+                    col_set.remove(col)
+                    left_dia_set.remove(row-col)
+                    right_dia_set.remove(row+col)
+            
+        backtrack(0)
         return res
-    def isValid(self, board:List[str], row:int, col:int):
-        rows = len(board)
-        cols = len(board[0])
-        #is current col valid
-        for r in range(row):
-            if board[r][col] == "Q":
-                return False
-        #is top left diagnoal valid
-        r = row -1
-        c = col -1
-        while r >= 0 and c >= 0:
-            if board[r][c] == "Q":
-                return False
-            r -= 1
-            c -= 1
-
-        #is top right diagnoal valid
-        rr = row -1
-        cr = col +1
-        while rr >= 0 and cr < len(board):
-            if board[rr][cr] == "Q":
-                return False
-            rr -= 1
-            cr +=1 
-
-        return True
+        
