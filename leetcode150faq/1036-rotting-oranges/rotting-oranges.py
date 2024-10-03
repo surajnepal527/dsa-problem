c78fafab2e2ca8a947ff8e9ce0df5288f2a3c4cc
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-10-03 16:56:44 -0600

    Time: 43 ms (91.06%) | Memory: 16.7 MB (22.01%) - LeetSync

--- leetcode150faq/1036-rotting-oranges/rotting-oranges.py
+++ leetcode150faq/1036-rotting-oranges/rotting-oranges.py
@@ -1,39 +1,27 @@
 from collections import deque
-class Solution(object):
-    def orangesRotting(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        row_len = len(grid)
-        col_len = len(grid[0])
-        queue = deque()
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        row_col_list = [(-1,0),(1,0),(0,-1), (0,1)]
+        que = deque()
         fresh_count = 0
-        #lets find first rotten orange
-        for row in range(row_len):
-            for col in range(col_len):
-                if grid[row][col] == 2:
-                    queue.append((row, col, 0))
-                elif grid[row][col] == 1:
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    que.append((r,c,0))
+                if grid[r][c] == 1:
                     fresh_count += 1
-
-
-        if fresh_count == 0:
-            return 0
-
-        directions = [(-1,0),(0,1),(1,0),(0,-1)]
-        stepd = 0
-        while queue:
-            cur_row, cur_col, steps = queue.popleft()
-            #max_step = max(max_step, steps)
-            
-            for dr, dc in directions:
-                new_row, new_col = dr+cur_row, dc+cur_col
-                if 0 <= new_row < row_len and 0 <= new_col < col_len and grid[new_row][new_col] == 1:
-                    grid[new_row][new_col] = 2
-                    queue.append((new_row, new_col, steps+1))
+        mt = 0
+        while que and fresh_count > 0:
+            ro, co, t = que.popleft()
+            for r1, c1 in row_col_list:
+                r_n = ro + r1
+                c_n = co + c1
+                if 0 <= r_n < rows and 0 <= c_n < cols and grid[r_n][c_n] == 1:
+                    grid[r_n][c_n] = 2
+                    que.append((r_n, c_n,t+1))
                     fresh_count -= 1
+                    mt = max(mt, t+1)
 
-        #check if there are any fresh orange left
-        return -1 if fresh_count > 0 else steps          
+        return mt if fresh_count == 0 else -1
         
