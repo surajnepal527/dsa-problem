a845e717b5f0270490c093c83fd97c643cf4d51d
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-10-04 06:40:32 -0600

    Time: 208 ms (69.04%) | Memory: 18.5 MB (10.66%) - LeetSync

--- leetcode150faq/417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ leetcode150faq/417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -2,8 +2,8 @@ from collections import deque
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         rows , cols = len(heights), len(heights[0])
-        visited_p = [[0 for _ in range(cols)] for _ in range(rows)]
-        visited_a = [[0 for _ in range(cols)] for _ in range(rows)]
+        visited_p = set()
+        visited_a = set()
         row_col_list = [(1,0),(-1,0),(0,1),(0, -1)]
         set_p = set()
         set_a = set()
@@ -14,29 +14,29 @@ class Solution:
                 if r == 0 or c == 0:
                     set_p.add((r,c))
                     que_p.append((r, c, heights[r][c]))
-                    visited_p[r][c] = -1
+                    visited_p.add((r,c))
                 if r == rows - 1 or c == cols -1:
                     set_a.add((r,c))
                     que_a.append((r, c, heights[r][c]))
-                    visited_a[r][c] = -1
+                    visited_a.add((r,c))
         while que_p:
             rqp, cqp, htqp = que_p.popleft()
             for rl, cl in row_col_list:
                 rn = rqp + rl
                 cn = cqp + cl
-                if 0 <= rn < rows and 0 <= cn < cols and heights[rn][cn] >= htqp and visited_p[rn][cn] != -1:
+                if 0 <= rn < rows and 0 <= cn < cols and heights[rn][cn] >= htqp and (rn, cn) not in visited_p:
                     set_p.add((rn, cn))
                     que_p.append((rn, cn , heights[rn][cn]))
-                    visited_p[rn][cn] = -1
+                    visited_p.add((rn, cn))
         while que_a:
             rqa, cqa, htqa = que_a.popleft()
             for rl, cl in row_col_list:
                 rn = rqa + rl
                 cn = cqa + cl
-                if 0 <= rn < rows and 0 <= cn < cols and heights[rn][cn] >= htqa and visited_a[rn][cn] != -1:
+                if 0 <= rn < rows and 0 <= cn < cols and heights[rn][cn] >= htqa and (rn, cn) not in visited_a:
                     set_a.add((rn, cn))
                     que_a.append((rn, cn, heights[rn][cn]))
-                    visited_a[rn][cn] = -1
+                    visited_a.add((rn ,cn))
         res = []
         for item in set_a:
             if item in set_p:
