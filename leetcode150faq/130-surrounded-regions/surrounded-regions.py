cbf7faaa243d17591c5a1a714a26a2071e98f462
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-10-05 06:15:28 -0600

    Time: 124 ms (41.45%) | Memory: 18.1 MB (39.92%) - LeetSync

--- leetcode150faq/130-surrounded-regions/surrounded-regions.py
+++ leetcode150faq/130-surrounded-regions/surrounded-regions.py
@@ -10,28 +10,37 @@ class Solution:
         que = deque()
         visited = set()
         row_col_list = [(1,0),(-1,0),(0,1),(0,-1)]
+        #just iterate over boundaries
+        #first row and last row
+        for c in range(cols):
+            if board[0][c] == "O":
+                board[0][c] = "NS"
+                que.append((0,c))
+            if board[rows-1][c] == "O":
+                board[rows-1][c] = "NS"
+                que.append((rows-1,c))
+        #first col and last col
         for r in range(rows):
-            for c in range(cols):
-                if r == 0 or c == 0 or r == rows- 1 or c == cols - 1:
-                    if board[r][c] == "O":
-                        board[r][c] = "NS"
-                        que.append((r,c))
-                    visited.add((r, c))
+            if board[r][0] == "O":
+                board[r][0] = "NS"
+                que.append((r, 0))
+            if board[r][cols-1] == "O":
+                board[r][cols-1] = "NS"
+                que.append((r,cols-1))
+
         while que:
             rq, cq = que.popleft()
             for rl, cl in row_col_list:
                 rn, cn = rq+rl, cq+cl
-                if 0<= rn < rows and 0<= cn < cols and (rn, cn) not in visited and board[rn][cn] == "O":
+                if 0<= rn < rows and 0<= cn < cols and board[rn][cn] == "O":
                     board[rn][cn] = "NS"
                     que.append((rn, cn))
         
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] != "NS":
+                if board[r][c] == "O":
                     board[r][c] = "X"
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c] == "NS":
+                elif board[r][c] == "NS":
                     board[r][c] = "O"
         
                 
