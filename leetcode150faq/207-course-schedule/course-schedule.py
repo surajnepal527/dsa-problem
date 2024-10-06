ade430023cf39d0cb06fb81f814dca0791c7da78
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-10-06 09:42:25 -0600

    Time: 112 ms (10.32%) | Memory: 18.6 MB (17.37%) - LeetSync

--- leetcode150faq/207-course-schedule/course-schedule.py
+++ leetcode150faq/207-course-schedule/course-schedule.py
@@ -1,33 +1,21 @@
-from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        #create a adj list
-        adj = defaultdict(list)
+        preMap = { i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
         visited = set()
-        current_path = set()
-        for c,p in prerequisites:
-            adj[p].append(c)
-        
-        def dfs(cur_course):
-            if cur_course in current_path:
-                return True
-            if cur_course in visited:
+        def dfs(crs):
+            if crs in visited:
                 return False
-            current_path.add(cur_course)
-            for prereq in adj[cur_course]:
-                if dfs(prereq):
-                    return True
-            current_path.remove(cur_course)
-            visited.add(cur_course)
-            return False
-        
-        for c in range(numCourses):
-            if c not in visited:
-                if dfs(c):
-                    return False
+            if preMap[crs] == []:
+                return True
+            visited.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):return False
+            visited.remove(crs)
+            preMap[crs] = []
+            return True
+        for crs in range(numCourses):
+            if not dfs(crs): return False
         return True
-            
-        
-        
-
         
