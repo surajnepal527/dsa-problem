f7ef3a4b7dc58edeba47cd21ebb3dcd8d9288879
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-10-15 10:17:58 -0600

    Time: 38 ms (28.02%) | Memory: 16.5 MB (67.53%) - LeetSync

--- leetcode150faq/70-climbing-stairs/climbing-stairs.py
+++ leetcode150faq/70-climbing-stairs/climbing-stairs.py
@@ -1,14 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1: return 1
-        if n == 2: return 2
-
-        ways_for_n_stair = [0] * (n+1)
-        ways_for_n_stair[0] = 0
-        ways_for_n_stair[1] = 1
-        ways_for_n_stair[2] = 2
+        if n <= 2 : return n
+        ways = [0] * (n+1)
+        ways[0] = 0
+        ways[1] = 1
+        ways[2] = 2
         for i in range(3, n+1):
-            ways_for_n_stair[i] = ways_for_n_stair[i-1] + ways_for_n_stair[i-2]
-
-        return ways_for_n_stair[n]
+            ways[i] = ways[i-1] + ways[i-2]
+        return ways[n]
         
