2a4a651dce39e3bf044ee51f51b8d9fabf5f2599
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-10-15 13:49:36 -0600

    Time: 54 ms (61.72%) | Memory: 16.5 MB (92.74%) - LeetSync

--- leetcode150faq/747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ leetcode150faq/747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,18 +1,12 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        #brute force top down approach
         k = len(cost)
-        memo = [-1] * (k+1)
-        def solve(pos):
-            if pos >= k :
-                return 0
-            if memo[pos] != -1: return memo[pos]
-            oneStep = cost[pos] + solve(pos+1)
-            twoStep = cost[pos] + solve(pos+2)
-            memo[pos] =  min(oneStep, twoStep)
-            return memo[pos]
-        start_zero = solve(0)
-        start_one = solve(1)
-        return min(start_zero, start_one)
+        first, second , third = cost[0], cost[1], 0
+        for i in range(2, k):
+            third = cost[i] + min(first, second)
+            first = second
+            second = third
+        return min(first, second)
+        
 
         
