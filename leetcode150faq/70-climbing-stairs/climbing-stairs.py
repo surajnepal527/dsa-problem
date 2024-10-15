ecbf897b6851796952e6b15747715b9fb1b2126b
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-10-15 10:02:22 -0600

    Time: 31 ms (77.16%) | Memory: 16.5 MB (23.72%) - LeetSync

--- leetcode150faq/70-climbing-stairs/climbing-stairs.py
+++ leetcode150faq/70-climbing-stairs/climbing-stairs.py
@@ -1,24 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-
-        memo = [-1]*(n+1)
+        memo = [-1] * (n +1)
         memo[0] = 1
         def solve(n):
-            if n < 0:
-                return 0
-            if memo[n] != -1:
-                return memo[n] 
+            if n < 0: return 0
+            if memo[n] != -1 : return memo[n]
             stepone = solve(n-1)
             steptwo = solve(n-2)
             memo[n] = stepone + steptwo
             return memo[n]
         return solve(n)
-    
-
-
-
-
-
-        
-        
-        
