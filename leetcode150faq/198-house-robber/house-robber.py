7c2b40ded83ce6bd883cd9b28b72d419611f1879
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-10-16 05:58:47 -0600

    Time: 34 ms (70.34%) | Memory: 16.6 MB (36.91%) - LeetSync

--- leetcode150faq/198-house-robber/house-robber.py
+++ leetcode150faq/198-house-robber/house-robber.py
@@ -2,9 +2,10 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         k = len(nums)
         if k == 1: return nums[0]
-        t =[0] * (k + 1)
-        t[0], t[1] = 0, nums[0]
+        first, second, third = 0, nums[0], 0
         for i in range(2, k+1):
-            t[i] = max(nums[i-1] + t[i-2], t[i-1])
-        return t[k]
+            third = max(nums[i-1] + first, second)
+            first = second
+            second = third
+        return third
         
