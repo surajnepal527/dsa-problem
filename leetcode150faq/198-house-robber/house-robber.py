bf1efebb9ac07e1e8f76c423122732f7487d78b7
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-10-16 05:57:09 -0600

    Time: 38 ms (41.27%) | Memory: 16.6 MB (36.91%) - LeetSync

--- leetcode150faq/198-house-robber/house-robber.py
+++ leetcode150faq/198-house-robber/house-robber.py
@@ -1,14 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         k = len(nums)
-        memo = [-1] * (k+1)
-        def solve(house):
-            if house >= k: return 0
-            if memo[house] != -1: return memo[house]
-            #keep
-            suma = nums[house] + solve(house+2)
-            #skip
-            sumb = solve(house+1)
-            memo[house] =  max(suma, sumb)
-            return memo[house]
-        return solve(0)
+        if k == 1: return nums[0]
+        t =[0] * (k + 1)
+        t[0], t[1] = 0, nums[0]
+        for i in range(2, k+1):
+            t[i] = max(nums[i-1] + t[i-2], t[i-1])
+        return t[k]
+        
