e5e81d0bf81cc87108874b4d6a5fe732dd10a616
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-10-18 13:38:16 -0600

    Time: 1053 ms (5.03%) | Memory: 120.1 MB (5.32%) - LeetSync

---
+++ leetcode150faq/647-palindromic-substrings/palindromic-substrings.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        #let write down solution for previous problem and see if we can do something with it for current problem
+        n = len(s)
+        memo = [[-1 for _ in range(n)] for _ in range(n)]
+        ans = 0
+        solved = set()
+        def solve(i, j):
+            if i >= j:
+                return 1
+            if memo[i][j] != -1:
+                return memo[i][j]
+            elif s[i] == s[j]:
+                memo[i][j] =  solve(i+1, j-1)
+                return memo[i][j]
+            else:
+                memo[i][j] = 0
+                return memo[i][j]
+        
+        for i in range(n):
+            for j in range(i, n):
+                if (i,j) not in solved and solve(i,j) == 1:
+                    solved.add((i,j))
+                    ans += 1
+        
+        return ans
+
+        
