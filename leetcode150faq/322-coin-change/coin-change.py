fe8d28226483565304b529c939499ccad19a817b
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-10-28 06:52:23 -0600

    Time: 1822 ms (5.00%) | Memory: 41.6 MB (11.41%) - LeetSync

--- leetcode150faq/322-coin-change/coin-change.py
+++ leetcode150faq/322-coin-change/coin-change.py
@@ -2,15 +2,15 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
        
-        dp = [[-1 for _ in range(amount+1)] for _ in range(len(coins))]
+        dp = [[-1 for _ in range(len(coins))] for _ in range(amount+1)]
         def solve(i, target):
             if target == 0: return 0
             if i >= len(coins) or target < 0: return float('inf')
-            if dp[i][target] != -1: return dp[i][target]
+            if dp[target][i] != -1: return dp[target][i]
             take = 1 + solve(i, target- coins[i]) if target - coins[i] >= 0 else float('inf')
             skip = solve(i+1, target)
-            dp[i][target] =  min(take, skip)
-            return dp[i][target]
+            dp[target][i] =  min(take, skip)
+            return dp[target][i]
         ans =  solve(0, amount)
         return ans if ans != float('inf') else -1
 
