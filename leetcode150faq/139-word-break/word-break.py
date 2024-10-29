f4a551903b4298af11f32c633a8993dd721d8dad
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-10-29 06:05:37 -0600

    Time: 0 ms (100.00%) | Memory: 16.8 MB (17.16%) - LeetSync

--- leetcode150faq/139-word-break/word-break.py
+++ leetcode150faq/139-word-break/word-break.py
@@ -1,18 +1,16 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         dp = [-1] * 302
-        def solve(i, st):
-            if i == len(s) : return True
+        def solve(st, i):
+            if i >= len(s): return True
             if dp[i] != -1: return dp[i]
-            if st in wordDict:
-                dp[i] = True
-                return dp[i]
-            for k in range(len(st)):
-                tmp = st[:k]
-                if tmp in wordDict and solve(i+len(tmp),st[len(tmp):]):
-                    dp[i] = True
-                    return dp[i]
+            for w in wordDict:
+                if st[:len(w)] == w:
+                    if solve(st[len(w):], i+len(w)):
+                        dp[i] = True
+                        return dp[i]
             dp[i] = False
             return dp[i]
+        return solve(s,0)
+
         
-        return solve(0, s)
