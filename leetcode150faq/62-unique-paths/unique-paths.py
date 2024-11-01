43577c8a7e78eaa372f54f6b3ee382c0cc9473b4
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-11-01 10:32:16 -0600

    Time: 0 ms (100.00%) | Memory: 16.6 MB (58.13%) - LeetSync

--- leetcode150faq/62-unique-paths/unique-paths.py
+++ leetcode150faq/62-unique-paths/unique-paths.py
@@ -1,14 +1,12 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dr = [(0,1),(1,0)]
-        t = [[-1 for _ in range(n)] for _ in range(m)]
-        def solve(r, c):
-            if r == m-1 and c == n-1: return 1
-            if r >= m or c >= n: return 0
-            if t[r][c] != -1: return t[r][c]
-            path_sum = 0
-            for nr, nc in dr:
-                path_sum += solve(r+nr, c+nc)
-            t[r][c] = path_sum
-            return t[r][c]
-        return solve(0, 0)
+        t = [ [0 for _ in range(n+1)] for _ in range(m+1)]
+        for r in range(m-1, -1, -1):
+            for c in range(n-1, -1, -1):
+                if r == m-1 and c == n-1:
+                    t[r][c] = 1
+                else:
+                    t[r][c] = t[r+1][c] + t[r][c+1]
+        return t[0][0]
+
+        
