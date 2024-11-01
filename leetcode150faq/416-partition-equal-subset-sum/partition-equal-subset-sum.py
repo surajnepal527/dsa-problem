6c4bafb70558e2deca5b2fefa38dae1dc5c66018
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-11-01 09:01:25 -0600

    Time: 598 ms (54.09%) | Memory: 19 MB (48.79%) - LeetSync

--- leetcode150faq/416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ leetcode150faq/416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,20 +1,15 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        #Lets start with approach one ie. recursion, where we have 2 choice take and not take
-        if sum(nums)%2: return False
-        totalSum = sum(nums)//2
-        memo = [[-1 for _ in range(200)] for _ in range(totalSum+1)]
-        def solve(idx, target):
-            if target == 0: return True
-            if memo[target][idx] != -1: return memo[target][idx]
-            if idx == 0: return nums[0] == target
-            not_take = solve(idx-1, target)
-            take = 0
-            if target >= nums[idx]:
-                take = solve(idx-1, target-nums[idx])
-            memo[target][idx] = take or not_take
-            return memo[target][idx]
-
-        return solve(len(nums)-1, totalSum)
+        if sum(nums) % 2 : return False
+        dp = set()
+        dp.add(0)
+        target = sum(nums)//2
+        for i in range(len(nums) -1,-1,-1):
+            nextDp = set()
+            for t in dp:
+                nextDp.add(t+nums[i])
+                nextDp.add(t)
+            dp = nextDp
+        return True if target in dp else False
 
         
