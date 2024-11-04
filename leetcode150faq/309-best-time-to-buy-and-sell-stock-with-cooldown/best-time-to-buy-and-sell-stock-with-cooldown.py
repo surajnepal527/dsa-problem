ecc295b0c116832461fb0021d0d4c941a31de08b
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-11-04 05:44:41 -0700

    Time: 7 ms (45.37%) | Memory: 18.2 MB (22.36%) - LeetSync

--- leetcode150faq/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ leetcode150faq/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,21 +1,17 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n, dp = len(prices), {}
+        def solve(idx, buying):
+            if idx >= n: return 0
+            if (idx, buying) in dp: return dp[(idx, buying)]
+            if buying:
+                buy_profit = solve(idx+1, not buying) - prices[idx]
+                not_buy_profit = solve(idx+1, buying)
+                dp[(idx, buying)] = max(buy_profit, not_buy_profit)
+            else:
+                sell_profit = solve(idx+2, not buying) + prices[idx]
+                not_sell_profit = solve(idx+1, buying)
+                dp[(idx, buying)] = max(sell_profit, not_sell_profit)
+            return dp[(idx, buying)]
+        return solve(0, True)
         
-        n = len(prices)
-        if n == 0 or n == 1:
-            return 0
-        t = [-1 for _ in range(50001)]
-        t[0] = 0
-        t[1] = max(prices[1]- prices[0], 0)
-        for i in range(2, n): 
-            t[i] = t[i-1]
-            for j in range(i):
-                today_profit = prices[i] - prices[j]
-                prev_profit = t[j-2] if j >= 2 else 0
-                t[i] = max(t[i], today_profit + prev_profit)
-
-        return t[n-1]
