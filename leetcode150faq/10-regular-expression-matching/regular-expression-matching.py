990f3b6255c9ac6b774783081d5c351da3185080
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-11-11 06:54:48 -0700

    Time: 4 ms (82.88%) | Memory: 16.9 MB (29.69%) - LeetSync

--- leetcode150faq/10-regular-expression-matching/regular-expression-matching.py
+++ leetcode150faq/10-regular-expression-matching/regular-expression-matching.py
@@ -1,7 +1,9 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        dp = {}
         def solve(i, j):
             if i == len(s) and j == len(p): return True
+            if (i,j) in dp: return dp[(i,j)]
             first_char_match = False
             if i < len(s) and j < len(p) and (s[i] == p[j] or p[j] == "."):
                 first_char_match = True
@@ -9,9 +11,11 @@ class Solution:
             if j+1 < len(p) and p[j+1] == "*":
                 not_take = solve(i, j+2)
                 take = first_char_match and solve(i+1, j)
-                return not_take or take
+                dp[(i,j)] = not_take or take
+                return dp[(i,j)]
 
-            return first_char_match and solve(i+1, j+1)
+            dp[(i,j)] = first_char_match and solve(i+1, j+1)
+            return dp[(i,j)]
         return solve(0,0)
 
 
