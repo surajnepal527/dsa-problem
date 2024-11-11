6b451ec6b798180594d72d1c326c2f69b44cae76
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-11-11 10:10:49 -0700

    Time: 15 ms (74.45%) | Memory: 17.7 MB (63.09%) - LeetSync

--- leetcode150faq/55-jump-game/jump-game.py
+++ leetcode150faq/55-jump-game/jump-game.py
@@ -1,17 +1,9 @@
-class Solution(object):
-    def canJump(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        if len(nums) == 1:
-            return True
-        j = len(nums) - 1
-        for i in range(len(nums) - 2, -1, -1):
-            if nums[i] >= j - i:
-                j = i
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums)-1
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i] >= goal - i:
+                goal = i
+        return goal == 0
+    
         
-        if j == 0:
-            return True
-        
-        return False
