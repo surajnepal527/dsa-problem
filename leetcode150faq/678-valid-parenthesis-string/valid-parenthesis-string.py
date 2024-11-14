035630fa982eb471b7e0750f14c05c8b40c52154
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-11-13 17:59:12 -0700

    Time: 0 ms (100.00%) | Memory: 16.8 MB (15.16%) - LeetSync

--- leetcode150faq/678-valid-parenthesis-string/valid-parenthesis-string.py
+++ leetcode150faq/678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,22 +1,26 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
         n = len(s)
-        t = [[False for _ in range(n+1)] for _ in range(n+1)]
-        t[n][0] = True
-        for i in range(n-1, -1, -1):
-            for open in range(n+1):
-                isValid = False
-                if s[i] == "*":
-                    isValid = t[i+1][open]
-                    if open > 0:
-                        isValid =  isValid or t[i+1][open-1]
-                    if open < n:
-                        isValid = isValid or t[i+1][open+1]
-                elif s[i] == "(":
-                    if open < n:
-                        isValid = t[i+1][open+1]
-                elif open > 0:
-                    isValid = t[i+1][open-1]
-                t[i][open] = isValid
-        return t[0][0]
+        openstk = []
+        askstk = []
+        for i in range(n):
+            if s[i] == "(":
+                openstk.append(i)
+            elif s[i] == "*":
+                askstk.append(i)
+            else:
+                if openstk:
+                    openstk.pop()
+                elif askstk:
+                    askstk.pop()
+                else:
+                    return False
+        
+        while openstk and askstk:
+            if openstk[-1] > askstk[-1]: return False
+            openstk.pop()
+            askstk.pop()
+        return True if not openstk else False
+                
+
         
