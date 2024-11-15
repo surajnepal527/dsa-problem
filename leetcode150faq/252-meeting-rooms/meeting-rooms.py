805459bebb96d9b2f1a5ff51f9fcac29ce4dd2d1
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-11-15 11:42:18 -0700

    Time: 2 ms (78.35%) | Memory: 18.8 MB (77.31%) - LeetSync

--- leetcode150faq/252-meeting-rooms/meeting-rooms.py
+++ leetcode150faq/252-meeting-rooms/meeting-rooms.py
@@ -4,10 +4,8 @@ class Solution:
         intervals.sort(key=lambda i:i[0])
         prev_start, prev_end = intervals[0][0], intervals[0][1]
         for cur_start, cur_end in intervals[1:]:
-            if cur_start < prev_end:
-                return False
-            else:
-                prev_start = cur_start
-                prev_end = cur_end
+            if cur_start < prev_end: return False
+            prev_start = cur_start
+            prev_end = cur_end
         return True
         
