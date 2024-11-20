3c11fa7c207de4b7fe70c41768bb60d19cc09b42
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-11-20 14:55:07 -0700

    Time: 0 ms (100.00%) | Memory: 16.6 MB (26.01%) - LeetSync

--- leetcode150faq/27-remove-element/remove-element.py
+++ leetcode150faq/27-remove-element/remove-element.py
@@ -1,22 +1,10 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        if not nums: return 0
-        val_cnt = 0
+        i = 0
         for num in nums:
-            if num == val: val_cnt += 1
-        if val_cnt == 0 : return len(nums)
-        i, j = 0 , len(nums) -1
-        while i < j:
-            if nums[j] == val:
-                j -= 1
-            elif nums[i] == val:
-                nums[i], nums[j] = nums[j], nums[i]
+            if num != val:
+                nums[i] = num
                 i += 1
-                j -= 1
-            else:
-                i += 1
-        return len(nums) - val_cnt
-
-
-
+        return i
+        
         
