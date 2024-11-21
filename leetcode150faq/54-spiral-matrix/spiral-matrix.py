408b08e32094d280296e5722a3516ee33b3722c1
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-11-21 07:57:50 -0700

    Time: 0 ms (100.00%) | Memory: 16.8 MB (12.59%) - LeetSync

--- leetcode150faq/54-spiral-matrix/spiral-matrix.py
+++ leetcode150faq/54-spiral-matrix/spiral-matrix.py
@@ -1,43 +1,34 @@
-class Solution(object):
-    def spiralOrder(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: List[int]
-        """
-        row = len(matrix)
-        col = len(matrix[0])
-        i = 0
-        j = 0
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix or not matrix[0]: return []
         output = []
-        while(row > 1 and col > 1):
+        top, bottom = 0, len(matrix)-1
+        left, right = 0 , len(matrix[0])-1
+        while top <= bottom and left <= right:
+            #left to right on top -->
+            for i in range(left, right+1):
+                output.append(matrix[top][i])
+            top += 1
 
-            for k in range(col - 1):
-                output.append(matrix[i][j])
-                j += 1
-            for k in range(row - 1):
-                output.append(matrix[i][j])
-                i += 1
-            for k in range(col - 1):
-                output.append(matrix[i][j])
-                j -= 1
-            for k in range(row - 1):
-                output.append(matrix[i][j])
-                i -= 1
-            i += 1
-            j += 1
-            row -= 2
-            col -= 2
-        
-        if row == 1 and col > 1:
-            for k in range(col):
-                output.append(matrix[i][j])
-                j += 1
-        if row > 1 and col == 1:
-            for k in range(row):
-                output.append(matrix[i][j])
-                i += 1
-        elif row == 1 and col == 1:
-            output.append(matrix[i][j])
-        
+            #top to bottom on right |
+            for j in range(top, bottom+1):
+                output.append(matrix[j][right])
+            right -= 1
+
+            #right to left on bottom
+            if top <= bottom:
+                for i in range(right, left-1, -1):
+                    output.append(matrix[bottom][i])
+                bottom -= 1
+
+            #botton to top on left
+            if left <= right:
+                for j in range(bottom , top-1, -1):
+                    output.append(matrix[j][left])
+                left += 1
+
         return output
+        
+        
+
         
