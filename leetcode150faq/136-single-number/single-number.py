3274a124cbe609081229cf83503bd8168109f2de
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-11-25 12:08:30 -0700

    Time: 3 ms (67.89%) | Memory: 18.7 MB (57.82%) - LeetSync

--- leetcode150faq/136-single-number/single-number.py
+++ leetcode150faq/136-single-number/single-number.py
@@ -2,6 +2,6 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         result = 0
         for num in nums:
-            result = result ^ num
+            result ^= num
         return result
         
