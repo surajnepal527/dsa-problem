d5d365bc35cc90498ed6d3813b9b90649a41ab4c
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-11-27 07:31:57 -0700

    Time: 7 ms (53.56%) | Memory: 18.2 MB (38.10%) - LeetSync

--- leetcode150faq/338-counting-bits/counting-bits.py
+++ leetcode150faq/338-counting-bits/counting-bits.py
@@ -1,15 +1,12 @@
-class Solution(object):
-    def countBits(self, n):
-        """
-        :type n: int
-        :rtype: List[int]
-        """
-        ans = []
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = [0] * (n+1)
         for i in range(n+1):
             if i == 0:
-                ans.append(0)
-            elif i % 2 == 0:
-                ans.append(ans[i//2])
+                res[i] = 0
+            elif i%2 == 0:
+                res[i] = res[i//2]
             else:
-                ans.append(ans[i//2] + 1)
-        return ans
+                res[i] = res[((i-1)//2)] + 1
+        return res
+            
