20732c40db14369efa3572a1f8a7539836dca332
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-12-22 18:25:35 -0700

    Time: 228 ms (85.65%) | Memory: 20 MB (44.01%) - LeetSync

--- leetcode150faq/200-number-of-islands/number-of-islands.py
+++ leetcode150faq/200-number-of-islands/number-of-islands.py
@@ -2,25 +2,22 @@ from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols, res = len(grid), len(grid[0]), 0
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
         que = deque()
-        def dfs():
-            while que:
-                pr, pc = que.popleft()
-                for dr, dc in [(1,0),(-1,0),(0,1),(0,-1)]:
-                    nr, nc = pr+dr, pc+dc
-                    if nr < 0 or nc < 0 or nr == rows or nc == cols or grid[nr][nc] != "1":
-                        continue
-                    grid[nr][nc] = "-1"
-                    que.append((nr, nc))
-        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
-                    grid[r][c] = "-1"
-                    que.append((r,c))
-                    dfs()
                     res += 1
+                    que.append((r,c))
+                    grid[r][c] = "0"
+                    while que:
+                        pr, pc = que.popleft()
+                        for dr, dc in directions:
+                            nr, nc = pr+dr, pc+dc
+                            if nr < 0 or nc< 0 or nr == rows or nc == cols or grid[nr][nc] != "1":
+                                continue
+                            que.append((nr, nc))
+                            grid[nr][nc] = "0"
         return res
-        
-        
-        
+
+
