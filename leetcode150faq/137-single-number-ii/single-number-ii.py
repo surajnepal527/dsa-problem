9f81b3e00d781a2bd42f889ab4f023dc7bdff444
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2024-12-25 11:29:55 -0700

    Time: 35 ms (23.20%) | Memory: 19.2 MB (13.89%) - LeetSync

--- leetcode150faq/137-single-number-ii/single-number-ii.py
+++ leetcode150faq/137-single-number-ii/single-number-ii.py
@@ -1,18 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        count = [0] * 32
+        count = [0]*32
         for num in nums:
             for i in range(32):
-                if num & (1<<i):
+                if num & 1<<i:
                     count[i] += 1
-        num = 0
+        result = 0
         for i in range(32):
             count[i] %= 3
-            num |= count[i]<<i
-        
+            result |= count[i] << i
         if count[31] == 1:
-            return num - (1<<32)
-        return num
+            return result - (1<<32)
+        return result
 
 
 
