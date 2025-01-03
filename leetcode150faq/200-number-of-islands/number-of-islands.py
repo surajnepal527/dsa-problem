8377844b3530394b32263ab78b272537559b549e
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-01-03 08:02:42 -0700

    Time: 236 ms (73.41%) | Memory: 20.1 MB (49.47%) - LeetSync

--- leetcode150faq/200-number-of-islands/number-of-islands.py
+++ leetcode150faq/200-number-of-islands/number-of-islands.py
@@ -1,18 +1,24 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows, cols, res = len(grid),len(grid[0]),0
+        rows, cols, res = len(grid), len(grid[0]), 0
         drs = [(1,0),(-1,0),(0,1),(0,-1)]
-        def solve(r, c):
-            for dr, dc in drs:
-                nr, nc = r+dr, c+dc
-                if nr < 0 or nc < 0 or nr == rows or nc == cols or grid[nr][nc] != "1":
-                    continue
-                grid[nr][nc] = "0"
-                solve(nr, nc)
+        que = deque()
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
-                    grid[r][c] = "0"
-                    solve(r,c)
+                    que.append((r,c))
+                    #grid[r][c] = "0"
                     res += 1
+                    while que:
+                        qr, qc = que.popleft()
+                        for dr,dc in drs:
+                            nr, nc = dr+qr, dc+qc
+                            if nr < 0 or nc < 0 or nr == rows or nc == cols or grid[nr][nc] != "1":
+                                continue
+                            grid[nr][nc] = "0"
+                            que.append((nr, nc))
         return res
+
+
+        
