db3bf230f875bd01d546aeef19ddbda20e7368c6
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-01-05 06:17:23 -0700

    Time: 25 ms (89.29%) | Memory: 20.4 MB (21.67%) - LeetSync

--- leetcode150faq/560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ leetcode150faq/560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,19 +1,12 @@
-class Solution(object):
-    def subarraySum(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        sum, ans = 0 , 0
-        map = {}
-        map[0] = 1
-        for num in nums:
-            sum += num
-            diff = sum - k
-            if diff in map:
-                ans += map[diff]
 
-            map[sum] = map.get(sum, 0)+1
-            
-        return ans
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefixSum = 0
+        prefixSumMap = {0:1}
+        count = 0
+        for num in nums:
+            prefixSum += num
+            diff = prefixSum - k
+            count += prefixSumMap.get(diff, 0)
+            prefixSumMap[prefixSum] = prefixSumMap.get(prefixSum,0) +1
+        return count
