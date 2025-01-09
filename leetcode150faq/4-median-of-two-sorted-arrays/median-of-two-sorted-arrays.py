4b905af75c97d398767fd9efe3d4c5d3463a55a1
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-01-09 06:21:40 -0700

    Time: 7 ms (18.32%) | Memory: 17.9 MB (37.42%) - LeetSync

--- leetcode150faq/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ leetcode150faq/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,27 +1,52 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums3, i , j, k = [0]*(len(nums1) + len(nums2)),0,0,0
-        while i < len(nums1) and j <len(nums2):
+        i, j, l1, l2, k = 0,0, len(nums1), len(nums2), 0
+        mididx = (l1+l2)//2
+        el1, el2 = 0, 0
+        even = (l1+l2)%2==0
+        idx1 = mididx -1
+        idx2 = mididx
+   
+        while i < len(nums1) and j < len(nums2):
             if nums1[i] < nums2[j]:
-                nums3[k] = nums1[i]
+                if k == idx1:
+                    el1 = nums1[i]
+                if k == idx2:
+                    el2 = nums1[i]
                 i += 1
             else:
-                nums3[k] = nums2[j]
+                if k == idx1:
+                    el1 = nums2[j]
+                if k == idx2:
+                    el2 = nums2[j]
                 j += 1
             k += 1
+
         while i < len(nums1):
-            nums3[k] = nums1[i]
-            i+= 1
+            if k == idx1:
+                el1 = nums1[i]
+            if k == idx2:
+                el2 = nums1[i]
+            i += 1
             k += 1
         while j < len(nums2):
-            nums3[k] = nums2[j]
+            if k == idx1:
+                el1 = nums2[j]
+            if k == idx2:
+                el2 = nums2[j]
             j += 1
             k += 1
-        
-        if len(nums3)%2==0:
-            n = len(nums3)//2
-            val1 , val2= nums3[n], nums3[n-1]
-            return (val1+val2)/2
+        if not even:
+            return el2/1
         else:
-            return nums3[(len(nums3)-1)//2]
+            return (el1+el2)/2
+
+
+
+
+                    
+
+            
+                
+
         
