b1b3240a30fc23fea212bf4f0a039ac84502226f
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-01-27 10:56:43 -0700

    Time: 13 ms (10.57%) | Memory: 17.7 MB (64.80%) - LeetSync

---
+++ leetcode150faq/12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,27 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        numMap = {
+            1000:"M",
+            900:"CM",
+            500: "D",
+            400: "CD",
+            100: "C",
+            90: "XC",
+            50: "L",
+            40: "XL",
+            10: "X",
+            9: "IX",
+            5: "V",
+            4: "IV",
+            1: "I"
+             }
+        result = ""
+        for n, r in numMap.items():
+            if not num: break
+            times = num//n
+            for i in range(times):
+                result += r
+            num = num%n
+
+        return result    
+
