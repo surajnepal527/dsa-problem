0b7891985e16f8bd48584cb112b889720dbb728f
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-02-03 06:50:19 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (19.17%) - LeetSync

--- leetcode150faq/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ leetcode150faq/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,16 +1,15 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []
+        if not digits: return []
+        digit_map = {"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
         res = []
-        digit_map = {"2": "abc","3":"def","4":"ghi","5":"jkl", "6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
-        def backtrack(cur_idx, ans):
+        def solve(cur_idx, ans):
             if len(ans) == len(digits):
                 res.append("".join(ans))
                 return
             for ch in digit_map[digits[cur_idx]]:
                 ans.append(ch)
-                backtrack(cur_idx+1, ans)
+                solve(cur_idx+1, ans)
                 ans.pop()
-        backtrack(0,[])
+        solve(0, [])
         return res
