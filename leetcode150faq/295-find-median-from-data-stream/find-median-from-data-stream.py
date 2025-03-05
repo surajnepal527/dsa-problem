4ae0d0a1797969697b410b339b0b51a886e2f5bc
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-03-05 06:12:09 -0700

    Time: 2217 ms (5.02%) | Memory: 39.3 MB (87.40%) - LeetSync

--- leetcode150faq/295-find-median-from-data-stream/find-median-from-data-stream.py
+++ leetcode150faq/295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,30 +1,23 @@
-import heapq
 class MedianFinder:
 
     def __init__(self):
-        self.max_heap = []
-        self.min_heap = []
+        self.array = []
+        self.size = 0
+        
 
     def addNum(self, num: int) -> None:
-        if not self.max_heap or num < -self.max_heap[0]:
-            heapq.heappush(self.max_heap, -num)
-        else:
-            heapq.heappush(self.min_heap, num)
-        #check validity
-        if abs(len(self.max_heap) - len(self.min_heap)) > 1:
-            if len(self.max_heap) > len(self.min_heap):
-                heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
-            else:
-                heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
-                
+        self.array.append(num)
+        self.size += 1
 
     def findMedian(self) -> float:
-        if len(self.max_heap) > len(self.min_heap):
-            return -self.max_heap[0]/1.0
-        elif len(self.min_heap) > len(self.max_heap):
-            return self.min_heap[0]/1.0
-        else:
-            return (-self.max_heap[0] + self.min_heap[0])/2.0
+        if self.size == 0:
+            return 0
+        self.array.sort()
+        if self.size%2 == 0:
+            mid = self.size//2
+            return (self.array[mid] + self.array[mid-1])/2.0
+        return self.array[self.size//2]
+        
 
 
 # Your MedianFinder object will be instantiated and called as such:
