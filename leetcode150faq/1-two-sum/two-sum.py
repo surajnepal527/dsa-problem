7aab28f55ff6106005c68f669c4dcb306e76a21c
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-03-23 07:04:19 -0600

    Time: 1821 ms (13.04%) | Memory: 18.3 MB (87.23%) - LeetSync

--- leetcode150faq/1-two-sum/two-sum.py
+++ leetcode150faq/1-two-sum/two-sum.py
@@ -1,11 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numMap = {nums[0]:0}
-        for i in range(1, len(nums)):
-            diff = target-nums[i]
-            if diff in numMap:
-                return [numMap[diff], i]
-            numMap[nums[i]] = i
-        
-        
+        #O(n2) solution
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums),1):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+        return []
         
