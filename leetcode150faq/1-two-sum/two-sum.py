0b375a172d283618033f6f19195666bb84169820
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-04-18 06:17:23 -0600

    Time: 0 ms (100.00%) | Memory: 18.8 MB (51.75%) - LeetSync

--- leetcode150faq/1-two-sum/two-sum.py
+++ leetcode150faq/1-two-sum/two-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_map = {}
-        for i in range(len(nums)):
-            comp = target - nums[i]
-            if comp in nums_map:
-                return [nums_map[comp],i]
-            elif nums[i] not in nums_map:
-                nums_map[nums[i]] = i
-        return []
+        num_map = {}
+        for index, num in enumerate(nums):
+            comp = target - num
+            if comp in num_map:
+                return [num_map[comp], index]
+            if num not in num_map:
+                num_map[num] = index
+        
         
