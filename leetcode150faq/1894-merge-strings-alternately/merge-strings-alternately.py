40e9dc444683b9e135f1fa33477e0e38e25e09ad
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-04-18 07:08:52 -0600

    Time: 35 ms (76.25%) | Memory: 17.6 MB (74.01%) - LeetSync

--- leetcode150faq/1894-merge-strings-alternately/merge-strings-alternately.py
+++ leetcode150faq/1894-merge-strings-alternately/merge-strings-alternately.py
@@ -7,12 +7,14 @@ class Solution:
             ans.append(word2[j])
             i += 1
             j += 1
-        while i < len(word1):
-            ans.append(word1[i])
-            i+= 1
-        while j < len(word2):
-            ans.append(word2[j])
-            j+= 1
+        ans.append(word1[i:])
+        ans.append(word2[j:])
+        # while i < len(word1):
+        #     ans.append(word1[i])
+        #     i+= 1
+        # while j < len(word2):
+        #     ans.append(word2[j])
+        #     j+= 1
         return "".join(ans)
 
         
