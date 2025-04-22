353d9b0efa36c1ff6f2d544dc9e9255297477cbf
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-04-22 06:42:42 -0600

    Time: 236 ms (81.44%) | Memory: 20 MB (82.95%) - LeetSync

--- leetcode150faq/200-number-of-islands/number-of-islands.py
+++ leetcode150faq/200-number-of-islands/number-of-islands.py
@@ -1,20 +1,21 @@
 from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows, cols, res = len(grid), len(grid[0]), 0
-        for r in range(rows):
-            for c in range(cols):
+        row_len, col_len, res = len(grid), len(grid[0]), 0
+        drs = [(0,1),(1,0),(0,-1),(-1,0)]
+        for r in range(row_len):
+            for c in range(col_len):
                 if grid[r][c] == "1":
-                    que = deque([(r,c)])
                     res += 1
-                    while que:
-                        qr, qc = que.popleft()
-                        for dr,dc in [(1,0),(-1,0),(0,1),(0,-1)]:
-                            nr, nc = dr+qr, dc+qc
-                            if nr < 0 or nc < 0 or nr == rows or nc == cols or grid[nr][nc] != "1":
+                    queue = deque([(r,c)])
+                    while queue:
+                        (qr, qc) = queue.popleft()
+                        for dr, dc in drs:
+                            nr, nc = qr+dr, qc+dc
+                            if nr < 0 or nc < 0 or nr == row_len or nc == col_len or grid[nr][nc] != "1":
                                 continue
                             grid[nr][nc] = "0"
-                            que.append((nr, nc))
+                            queue.append((nr, nc))
         return res
 
 
