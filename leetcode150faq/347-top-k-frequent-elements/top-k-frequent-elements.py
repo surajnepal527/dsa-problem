a608f4aeabaa49a4eaa3a5e118724079d0536fc8
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-06-05 15:53:29 -0600

    Time: 3 ms (91.13%) | Memory: 21.3 MB (40.19%) - LeetSync

--- leetcode150faq/347-top-k-frequent-elements/top-k-frequent-elements.py
+++ leetcode150faq/347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,20 +1,14 @@
+import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        tmp = [[] for _ in range(len(nums)+1)] 
-        ans = []
-        freq_map = {}
+        res, max_heap, freq_map = [], [],{}
         for num in nums:
-            freq_map[num] = freq_map.get(num, 0) + 1
+            freq_map[num] = freq_map.get(num,0)+1
+        for key in freq_map:
+            value = freq_map[key]
+            heapq.heappush(max_heap,(-value, key))
+        for i in range(k):
+            neg_value, num = heapq.heappop(max_heap)
+            res.append(num)
+        return res
         
-        for num, freq in freq_map.items():
-            tmp[freq].append(num)
-        
-        for i in range(len(nums), -1, -1):
-            for num in tmp[i]:
-                if k > 0:
-                    ans.append(num)
-                    k -= 1
-                else:
-                    return ans
-        return ans
-
