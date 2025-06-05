9d82fbd7f1505984344768bb488930c5833dcf83
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-06-05 09:43:46 -0600

    Time: 15 ms (81.98%) | Memory: 17.9 MB (53.10%) - LeetSync

--- leetcode150faq/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ leetcode150faq/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,19 +1,13 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        #approach was good but it seems more complicated.
-        #lets just try to set and so the same 
-        #as soon as we see cur char already exist in set before we even insert it
-        #we try to remove all elmement from let till we remove the curent element
-        #then we add new char and check for size
-        char_set = set()
-        left, res = 0, 0
-        for right in range(len(s)):
-            while s[right] in char_set:
+        res, char_set =0, set()
+        left = 0
+        for right, char in enumerate(s):
+            while char in char_set:
                 char_set.remove(s[left])
                 left += 1
-            char_set.add(s[right])
+            char_set.add(char)    
             res = max(res, len(char_set))
         return res
-
 
         
