5ba2a2e5a9227cd8e160c5ee00276920db683d54
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-06-07 12:55:33 -0600

    Time: 3 ms (82.34%) | Memory: 17.9 MB (44.76%) - LeetSync

--- leetcode150faq/1036-rotting-oranges/rotting-oranges.py
+++ leetcode150faq/1036-rotting-oranges/rotting-oranges.py
@@ -1,27 +1,26 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        row_col_list = [(-1,0),(1,0),(0,-1), (0,1)]
-        que = deque()
+        row_len, col_len = len(grid),len(grid[0])
+        drs = [(1,0),(0,1),(-1,0),(0,-1)]
+        queue = deque()
+        max_count = 0
         fresh_count = 0
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 2:
-                    que.append((r,c,0))
+        for r in range(row_len):
+            for c in range(col_len):
                 if grid[r][c] == 1:
                     fresh_count += 1
-        mt = 0
-        while que and fresh_count > 0:
-            ro, co, t = que.popleft()
-            for r1, c1 in row_col_list:
-                r_n = ro + r1
-                c_n = co + c1
-                if 0 <= r_n < rows and 0 <= c_n < cols and grid[r_n][c_n] == 1:
-                    grid[r_n][c_n] = 2
-                    que.append((r_n, c_n,t+1))
-                    fresh_count -= 1
-                    mt = max(mt, t+1)
+                if grid[r][c] == 2:
+                    queue.append((r, c, 0))
 
-        return mt if fresh_count == 0 else -1
-        
+        while queue and fresh_count > 0:
+            qr, qc , step = queue.popleft()
+            for dr, dc in drs:
+                nr, nc = qr+dr, qc+dc
+                if nr < 0 or nr >= row_len or nc < 0 or nc >= col_len or grid[nr][nc] != 1:
+                    continue
+                grid[nr][nc] = 2
+                queue.append((nr,nc,step+1))
+                fresh_count -= 1
+                max_count = max(max_count,step+1)
+        return max_count if fresh_count == 0 else -1
