70d716185d10a406854bf3be78ed7713deb7cd8b
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-06-07 10:21:58 -0600

    Time: 233 ms (86.26%) | Memory: 20.3 MB (58.07%) - LeetSync

--- leetcode150faq/200-number-of-islands/number-of-islands.py
+++ leetcode150faq/200-number-of-islands/number-of-islands.py
@@ -1,21 +1,26 @@
 from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        row_len, col_len, res = len(grid), len(grid[0]), 0
-        drs = [(0,1),(1,0),(0,-1),(-1,0)]
+        drs = [(1,0),(0,1),(-1,0),(0,-1)]
+        row_len, col_len = len(grid), len(grid[0])
+        res = 0
         for r in range(row_len):
             for c in range(col_len):
                 if grid[r][c] == "1":
+                    grid[r][c] = "0"
                     res += 1
                     queue = deque([(r,c)])
                     while queue:
-                        (qr, qc) = queue.popleft()
+                        qr, qc = queue.popleft()
                         for dr, dc in drs:
                             nr, nc = qr+dr, qc+dc
-                            if nr < 0 or nc < 0 or nr == row_len or nc == col_len or grid[nr][nc] != "1":
+                            #main part of the code
+                            #boundary conditin and #is there land if not ignore
+                            if nr < 0 or nr >= row_len or nc<0 or nc>= col_len or grid[nr][nc] != "1":
                                 continue
                             grid[nr][nc] = "0"
                             queue.append((nr, nc))
+        
         return res
 
 
