7aaac5bbec9f83f66cd5b933b4f634f37f9cf7e5
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-06-07 07:30:58 -0600

    Time: 157 ms (36.35%) | Memory: 78.7 MB (27.35%) - LeetSync

--- leetcode150faq/146-lru-cache/lru-cache.py
+++ leetcode150faq/146-lru-cache/lru-cache.py
@@ -1,57 +1,48 @@
 class Node:
-    def __init__(self, key=0, val=0,next=None, prev=None):
-        self.key=key
-        self.val=val
-        self.prev = prev
-        self.next = next
+    def __init__(self, key=0, val=0):
+        self.key = key
+        self.val = val
+        self.prev = self.next = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity
+        self.capacity = capacity
         self.cache = {}
-        self.head , self.tail = Node(), Node()
-        self.head.next = self.tail
-        self.tail.prev = self.head
+        self.left = self.right = Node(0,0)
+        self.left.next, self.right.prev = self.right, self.left
+        
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            node = self.cache[key]
-            self.moveNodeToHead(node)
-            return node.val
-        else:
-            return -1
+            self.remove(self.cache[key]) #remove from existing location
+            self.insert(self.cache[key]) # add to the right most position
+            return self.cache[key].val
+        return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            node = self.cache[key]
-            node.val = value
-            self.moveNodeToHead(node)
-        else:
-            if len(self.cache) == self.cap:
-                self.removeLRUNode()
-            newNode = Node(key=key, val=value)
-            self.cache[key] = newNode
-            self.addNodeToHead(newNode)
+            self.remove(self.cache[key])
+        new_node = Node(key, value)
+        self.insert(new_node)
+        self.cache[key] = new_node
+        if len(self.cache) > self.capacity:
+            lru = self.left.next
+            del self.cache[lru.key]
+            self.remove(lru)
+            
+        
+    def remove(self ,node:Node):
+        prev, next = node.prev, node.next
+        prev.next = next
+        next.prev = prev
 
-    
-    def removeNode(self, node):
-        node_next, node_prev = node.next, node.prev
-        node.prev.next, node.next.prev = node_next, node_prev
 
-    def removeLRUNode(self):
-        lru = self.tail.prev
-        self.removeNode(lru)
-        del self.cache[lru.key]
+    def insert(self, node:Node):
+        prev , next = self.right.prev , self.right
+        prev.next = next.prev = node
+        node.next , node.prev = next, prev
 
-    def addNodeToHead(self, node):
-        node.prev, node.next = self.head, self.head.next
-        self.head.next.prev = node
-        self.head.next = node    
-
-    def moveNodeToHead(self, node):
-        self.removeNode(node)
-        self.addNodeToHead(node)
         
 
 
