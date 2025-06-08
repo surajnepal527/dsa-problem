d22e63a7cd9175042c46a2ecc601c13e1a8e44a1
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-06-08 16:43:08 -0600

    Time: 224 ms (96.38%) | Memory: 20.1 MB (82.61%) - LeetSync

--- leetcode150faq/200-number-of-islands/number-of-islands.py
+++ leetcode150faq/200-number-of-islands/number-of-islands.py
@@ -1,17 +1,23 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         row_len, col_len = len(grid), len(grid[0])
         drs = [(1,0),(0,1),(-1,0),(0,-1)]
         res = 0
-        def dfs(row:int, col:int):
-            if row < 0 or col < 0 or row >= row_len or col >= col_len or grid[row][col] != "1":
-                return
-            grid[row][col] = "0"
-            for dr,dc in drs:
-                dfs(row+dr, col+dc) 
+        queue = deque([])
         for r in range(row_len):
             for c in range(col_len):
                 if grid[r][c] == "1":
                     res += 1
-                    dfs(r, c)
+                    queue.append((r,c))
+                    while queue:
+                        qr, qc = queue.popleft()
+                        grid[qr][qc] = "0"
+                        for dr, dc in drs:
+                            nr, nc = qr+dr, qc+dc
+                            if nr < 0 or nr >= row_len or nc < 0 or nc >= col_len or grid[nr][nc] != "1":
+                                continue
+                            grid[nr][nc] = "0"
+                            queue.append((nr, nc))
+                        
         return res
