b333ef69f1f9b05c99642b8952e956dad5a7986c
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-06-10 05:20:59 -0600

    Time: 23 ms (6.54%) | Memory: 18 MB (83.24%) - LeetSync

--- leetcode150faq/125-valid-palindrome/valid-palindrome.py
+++ leetcode150faq/125-valid-palindrome/valid-palindrome.py
@@ -1,24 +1,25 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        left = 0
-        right = len(s) - 1
+        def isValidAscii(ch:chr)->bool:
+            ascii_val = ord(ch)
+            return (ascii_val >= ord('0') and ascii_val <= ord('9')) or (ascii_val >= ord('a') and ascii_val <= ord('z')) or (ascii_val >= ord('A') and ascii_val <= ord('Z'))
+        left, right = 0, len(s)-1
         while left < right:
-            while not self.isValidAscii(s[left]) and left < right:
+            while not isValidAscii(s[left]) and left < right:
                 left += 1
-            while not self.isValidAscii(s[right]) and left < right:
-                right -= 1
+            while not isValidAscii(s[right]) and left < right:
+                right -=1
             if left == right:
                 return True
-            left_char = s[left].lower()
-            right_char = s[right].lower()
+            left_char = (s[left]).lower()
+            right_char = (s[right]).lower()
             if left_char != right_char:
                 return False
             left += 1
             right -= 1
-        
         return True
 
 
-    def isValidAscii(self, char: chr) -> bool:
-        ascii_val = ord(char)
-        return (ascii_val >= ord('0') and ascii_val <= ord('9')) or (ascii_val >= ord('a') and ascii_val <= ord('z')) or (ascii_val >= ord('A') and ascii_val <= ord('Z'))    
+    
+
+        
