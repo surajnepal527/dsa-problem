d5f233efb931831c2a9398b333bc230f206aae59
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-06-12 06:44:39 -0600

    Time: 7 ms (90.39%) | Memory: 20.1 MB (70.34%) - LeetSync

--- leetcode150faq/23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ leetcode150faq/23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,31 +5,37 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists) == 0:
+        if  not list or len(lists) == 0:
             return None
         while len(lists) > 1:
-            mergedLists = []
-            for i in range(0,len(lists), 2):
+            mergedList = []
+            for i in range(0,len(lists),2):
                 l1 = lists[i]
                 l2 = lists[i+1] if i+1 < len(lists) else None
-                mergedLists.append(self.mergeList(l1, l2))
-            lists = mergedLists
+                mergedList.append(self.merge(l1, l2))
+            lists = mergedList
         return lists[0]
     
-    def mergeList(self, l1, l2):
-        dummy = ListNode()
-        tail = dummy
+    def merge(self, l1:Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummyNode = ListNode()
+        cur = dummyNode
         while l1 and l2:
-            if l1.val<= l2.val:
-                tail.next = l1
+            if l1.val < l2.val:
+                cur.next = l1
                 l1 = l1.next
             else:
-                tail.next = l2
+                cur.next = l2
                 l2 = l2.next
-            tail = tail.next
-        if l1:
-            tail.next = l1
-        if l2:
-            tail.next = l2
-        return dummy.next
+            cur = cur.next
+        while l1:
+            cur.next = l1
+            cur = cur.next
+            l1 = l1.next
+        while l2:
+            cur.next = l2
+            cur = cur.next
+            l2 = l2.next
+        return dummyNode.next
+
+
         
