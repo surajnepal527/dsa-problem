54a29fca063a243743eeb85fd4b14452aaabf7fc
surajnepal527 <164102900+surajnepal527@users.noreply.github.com>
2025-06-12 17:48:55 -0600

    Time: 140 ms (60.43%) | Memory: 39.3 MB (76.34%) - LeetSync

--- leetcode150faq/295-find-median-from-data-stream/find-median-from-data-stream.py
+++ leetcode150faq/295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -2,24 +2,28 @@ import heapq
 class MedianFinder:
 
     def __init__(self):
-        self.small = []
-        self.large = []
+        self.max_left_heap = []
+        self.min_right_heap = []
+        
 
     def addNum(self, num: int) -> None:
-        if len(self.small) == 0 or num < -self.small[0]:
-            heapq.heappush(self.small, -num)
+        if len(self.max_left_heap) == 0 or num < -self.max_left_heap[0]:
+            heapq.heappush(self.max_left_heap, -num)
         else:
-            heapq.heappush(self.large, num)
-        #balance heap
-        if len(self.large) > len(self.small):
-            heapq.heappush(self.small, -heapq.heappop(self.large))
-        if len(self.small) - len(self.large) > 1:
-            heapq.heappush(self.large, -heapq.heappop(self.small))
+            heapq.heappush(self.min_right_heap, num)
+        ##balancing heap
+        if len(self.min_right_heap) > len(self.max_left_heap):
+            heapq.heappush(self.max_left_heap, -heapq.heappop(self.min_right_heap))
+        while len(self.max_left_heap) - len(self.min_right_heap) > 1:
+            heapq.heappush(self.min_right_heap, -heapq.heappop(self.max_left_heap))
+        
+        
 
     def findMedian(self) -> float:
-        if len(self.small) == len(self.large):
-            return (-self.small[0]+self.large[0])/2.0
-        return -self.small[0]/1.0
+        if len(self.max_left_heap) == len(self.min_right_heap):
+            return (-self.max_left_heap[0] + self.min_right_heap[0])/2.0
+        else:
+           return -self.max_left_heap[0]/1.0
         
 
 
